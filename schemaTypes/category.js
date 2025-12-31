9c1cbf47fe5b285f9caf249b0df5e5ef7cd86ff9
Rohan chauhan <rohanchauhan196@gmail.com>
2025-12-30 22:33:25 -0800

    Initial commit of Sanity Studio

---
+++ schemaTypes/category.js
@@ -0,0 +1,9 @@
+export default {
+    name: 'category',
+    title: 'Category',
+    type: 'document',
+    fields: [
+        { name: 'title', title: 'Title', type: 'string' },
+        { name: 'description', title: 'Description', type: 'text' },
+    ],
+}
