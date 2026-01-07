6d2fb7d266b12568ac5b4d08323b6398dca8b25c
Rohan chauhan <rohanchauhan196@gmail.com>
2026-01-07 11:35:45 +0530

    ajsf

--- schemaTypes/page.js
+++ schemaTypes/page.js
@@ -1,14 +1,29 @@
 export default {
   name: 'page',
-  title: 'Page',
+  title: 'Static Pages (Home, About, etc.)',
   type: 'document',
   fields: [
     { name: 'title', title: 'Title', type: 'string' },
-    { name: 'slug', title: 'Slug', type: 'slug', options: { source: 'title' } },
+    {
+      name: 'slug',
+      title: 'Page',
+      type: 'string',
+      description: 'Select which static page this document represents.',
+      options: {
+        list: [
+          { title: 'Home Page', value: 'home' },
+          { title: 'About Us', value: 'about' },
+          { title: 'Contact Us', value: 'contact' },
+          { title: 'All Services Page', value: 'services' },
+          { title: 'All Packages Page', value: 'packages' },
+        ],
+      },
+      validation: (Rule) => Rule.required(),
+    },
     { name: 'content', title: 'Content', type: 'blockContent' },
     { name: 'seo', title: 'SEO', type: 'seo' },
   ],
   preview: {
-    select: { title: 'title', subtitle: 'slug.current' },
+    select: { title: 'title', subtitle: 'slug' },
   },
 }
